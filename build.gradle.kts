dbc8bc02e1283c39c49d6b8c9717bb4539d2af94
lazyhat <sevapetrov2016p@gmail.com>
2026-03-21 01:15:45 +0300

    set up architectury with forge

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,136 +1,74 @@
 @file:Suppress("PropertyName")
 
-import org.slf4j.event.Level
+import org.gradle.kotlin.dsl.provideDelegate
 import kotlin.reflect.KProperty
 
-fun File.parseProperties(): Map<String, String> =
-    readLines()
-        .mapNotNull { it.indexOf('=').takeIf { i -> i != -1 }?.let { v -> v to it } }
-        .associate { (index, str) -> str.substring(0, index) to str.substring(index + 1) }
-
-val modPropertiesFile = file("mod.properties")
-val modPropertiesDelegate = modPropertiesFile.parseProperties()
-
-val kotlin_version by modPropertiesDelegate
-val mod_id by modPropertiesDelegate
-val mod_name by modPropertiesDelegate
-val mod_license by modPropertiesDelegate
-val mod_authors by modPropertiesDelegate
-val mod_description by modPropertiesDelegate
-val mod_version by modPropertiesDelegate
-val mod_group_id by modPropertiesDelegate
-val minecraft_version by modPropertiesDelegate
-val minecraft_version_range by modPropertiesDelegate
-val forge_version by modPropertiesDelegate
-val kotlin_for_forge_version by modPropertiesDelegate
-val forge_version_range by modPropertiesDelegate
-val loader_version_range by modPropertiesDelegate
-val parchment_mappings_version by modPropertiesDelegate
-val parchment_minecraft_version by modPropertiesDelegate
+//operator fun Provider<String>.getValue(ref: Any?, prop: KProperty<*>): String = this.get()
+//
+//fun File.parseProperties(): Map<String, String> =
+//    readLines()
+//        .mapNotNull { it.indexOf('=').takeIf { i -> i != -1 }?.let { v -> v to it } }
+//        .associate { (index, str) -> str.substring(0, index) to str.substring(index + 1) }
+//
+//val modPropertiesFile = file("config/mod.properties")
+//val modPropertiesDelegate = extra.properties.mapValues { (_, v) -> v.toString() }
+//
+//val mod_id by modPropertiesDelegate
+//val mod_name by modPropertiesDelegate
+//val mod_license by modPropertiesDelegate
+//val mod_authors by modPropertiesDelegate
+//val mod_description by modPropertiesDelegate
+//val mod_version by modPropertiesDelegate
+//val mod_group_id by modPropertiesDelegate
+//val minecraft_version by libs.versions.minecraft
+//val minecraft_version_range by modPropertiesDelegate
+//val forge_version by modPropertiesDelegate
+//val forge_version_range by modPropertiesDelegate
+//val loader_version_range by modPropertiesDelegate
+//val parchment_mappings_version by modPropertiesDelegate
+//val parchment_minecraft_version by modPropertiesDelegate
 
 plugins {
     idea
     alias(libs.plugins.kotlin)
-    alias(libs.plugins.neoforge)
+    alias(libs.plugins.architectury.loom)
+    alias(libs.plugins.architectury.plugin)
 }
-
-tasks.withType(Wrapper::class) {
-    distributionType = Wrapper.DistributionType.BIN
-}
-
-version = mod_version
-group = mod_group_id
 
 repositories {
-    mavenCentral()
-    //maven("https://thedarkcolour.github.io/KotlinForForge/") { name = "Kotlin for Forge" }
-    maven("https://maven.neoforged.net") {
-        name = "NeoForge"
-        content {
-            includeGroup("net.neoforged")
-        }
-    }
-    maven("https://maven.minecraftforge.net/") {
-        name = "Forge"
-    }
+    maven("https://maven.parchmentmc.org/")
 }
 
-base.archivesName = mod_id
+//loom {
+//    forge {
+//        mixinConfig("compuktercraft.mixins.json")
+//    }
+//}
 
-java.toolchain.languageVersion = JavaLanguageVersion.of(17)
+
+architectury {
+    minecraft = libs.versions.minecraft.get()
+
+    platformSetupLoomIde()
+    forge()
+}
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
-    implementation(kotlin("stdlib"))
-    implementation(kotlin("reflect"))
+    minecraft(libs.minecraft)
 
-    runtimeOnly(kotlin("stdlib"))
-    runtimeOnly(kotlin("stdlib-jdk8"))
-    runtimeOnly(kotlin("reflect"))
+    mappings(loom.layered { officialMojangMappings(); parchment(libs.parchment.for1v20v1) })
 
-//    add("clientAdditionalRuntimeClasspath", kotlin("stdlib"))
-//    add("clientAdditionalRuntimeClasspath", kotlin("reflect"))
-//
-//    add("additionalRuntimeClasspath", kotlin("stdlib"))
-//    add("additionalRuntimeClasspath", kotlin("reflect"))
+    modImplementation(libs.architectury.forge)
 
-    jarJar(kotlin("stdlib-jdk8"))
-    jarJar(kotlin("stdlib"))
-    jarJar(kotlin("reflect"))
+    forge(libs.forge)
 }
 
-legacyForge {
-    version = "$minecraft_version-$forge_version"
-
-    parchment {
-        mappingsVersion = parchment_mappings_version
-        minecraftVersion = parchment_minecraft_version
-    }
-
-    runs {
-        register("client") {
-            client()
-            systemProperty("forge.enabledGameTestNamespaces", mod_id)
-        }
-
-//        register("server") {
-//            server()
-//            programArgument("--nogui")
-//            systemProperty("forge.enabledGameTestNamespaces", mod_id)
-//        }
-//
-//        register("gameTestServer") {
-//            type = "gameTestServer"
-//            systemProperty("forge.enabledGameTestNamespaces", mod_id)
-//        }
-
-//        register("data") {
-//            data()
-//
-//            programArguments.addAll(
-//                "--mod", mod_id,
-//                "--all",
-//                "--output", file("src/generatedCtqgjke/resources/").absolutePath,
-//                "--existing", file("src/main/resources/").absolutePath,
-//            )
-//        }
-
-        configureEach {
-            systemProperty("forge.logging.markers", "REGISTRIES")
-            logLevel = Level.DEBUG
-        }
-    }
-
-    mods {
-        register(mod_id) {
-            sourceSet(sourceSets.main.get())
-        }
-    }
-}
-
-var generateModMetadata =
+val generateModMetadata =
     tasks.register("generateModMetadata", ProcessResources::class) {
-        val replaceProperties = modPropertiesFile.parseProperties()
+        val modPropertiesFile = file("config/mod.properties")
+        val replaceProperties = modPropertiesFile.readLines()
+            .mapNotNull { it.indexOf('=').takeIf { i -> i != -1 }?.let { v -> v to it } }
+            .associate { (index, str) -> str.substring(0, index) to str.substring(index + 1) }
         val from = file("src/main/resources")
         val intoDir = file("build/generated/resources")
 
@@ -147,24 +85,10 @@ var generateModMetadata =
         expand(replaceProperties)
     }
 
-tasks.named("processResources") {
+tasks.named<ProcessResources>("processResources") {
     dependsOn(generateModMetadata)
 }
 
 sourceSets.main {
-    resources.setSrcDirs(listOf("build/generated/resources"))
-}
-
-tasks.named<Jar>("jar") {
-    archiveClassifier.set("")
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-
-    finalizedBy("reobfJar")
-}
-
-idea {
-    module {
-        isDownloadJavadoc = true
-        isDownloadSources = true
-    }
+    resources.setSrcDirs(generateModMetadata.get().outputs.files)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,20 @@
+pluginManagement {
+    repositories {
+        maven("https://maven.architectury.dev/")
+        maven("https://maven.fabricmc.net/")
+        maven("https://maven.minecraftforge.net/")
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("config/libs.versions.toml"))
+        }
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
     id("de.fayard.refreshVersions") version "0.60.6"
