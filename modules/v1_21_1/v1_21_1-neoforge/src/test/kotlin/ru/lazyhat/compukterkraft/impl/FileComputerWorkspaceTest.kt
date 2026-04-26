de7b8bdfe3cde6aaab15c30c455352fc01889418
lazyhat <sevapetrov2016p@gmail.com>
2026-04-26 15:15:45 +0300

    chore: rename test files and update package declaration for consistency

--- modules/v1_21_1/v1_21_1-neoforge/src/test/kotlin/ru/lazyhat/compukterkraft/impl/FileComputerWorkspaceTest.kt
+++ modules/v1_21_1/v1_21_1-neoforge/src/test/kotlin/ru/lazyhat/compukterkraft/impl/FileComputerWorkspaceTest.kt
@@ -16,6 +16,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+package ru.lazyhat.compukterkraft.impl
 
 import ru.lazyhat.compukterkraft.core.computer.vm.ComputerWorkspaceHost
 import ru.lazyhat.compukterkraft.core.computer.vm.ComputerWorkspaceInitializer
