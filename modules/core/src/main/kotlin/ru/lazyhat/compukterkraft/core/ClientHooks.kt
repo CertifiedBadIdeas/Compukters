d04b6160371ca21e498646b6c0a0c3cda4e0cb9f
lazyhat <sevapetrov2016p@gmail.com>
2026-05-11 08:02:22 +0300

    Remove stale client hook scaffolding

--- modules/core/src/main/kotlin/ru/lazyhat/compukterkraft/core/ClientHooks.kt
+++ modules/core/src/main/kotlin/ru/lazyhat/compukterkraft/core/ClientHooks.kt
@@ -18,6 +18,8 @@
  */
 package ru.lazyhat.compukterkraft.core
 
+import ru.lazyhat.compukterkraft.core.gui.FrameInfo
+
 /**
  * Event listeners for client-only code.
  *
@@ -27,140 +29,10 @@ package ru.lazyhat.compukterkraft.core
  */
 object ClientHooks {
     fun onTick() {
-        _root_ide_package_.ru.lazyhat.compukterkraft.core.gui.FrameInfo
-            .onTick()
+        FrameInfo.onTick()
     }
 
     fun onRenderTick() {
-        // PauseAwareTimer.tick(Minecraft.getInstance().isPaused())
-        _root_ide_package_.ru.lazyhat.compukterkraft.core.gui.FrameInfo
-            .onRenderTick()
+        FrameInfo.onRenderTick()
     }
-
-// 	fun onWorldUnload() {
-// 		MonitorRenderState.destroyAll()
-// 		SpeakerManager.reset()
-// 	}
-//
-// 	fun onDisconnect() {
-// 		ClientPocketComputers.reset()
-// 	}
-//
-// 	fun drawHighlight(transform: PoseStack?, bufferSource: MultiBufferSource?, camera: Camera?, hit: BlockHitResult?): Boolean {
-// 		return CableHighlightRenderer.drawHighlight(transform, bufferSource, camera, hit)
-// 				|| MonitorHighlightRenderer.drawHighlight(transform, bufferSource, camera, hit)
-// 	}
-//
-// 	fun onRenderHeldItem(
-// 		transform: PoseStack?, render: MultiBufferSource?, lightTexture: Int, hand: InteractionHand?,
-// 		pitch: Float, equipProgress: Float, swingProgress: Float, stack: ItemStack
-// 	): Boolean {
-// 		if (stack.getItem() is PocketComputerItem) {
-// 			PocketItemRenderer.INSTANCE.renderItemFirstPerson(
-// 				transform,
-// 				render,
-// 				lightTexture,
-// 				hand,
-// 				pitch,
-// 				equipProgress,
-// 				swingProgress,
-// 				stack
-// 			)
-// 			return true
-// 		}
-// 		if (stack.getItem() is PrintoutItem) {
-// 			PrintoutItemRenderer.INSTANCE.renderItemFirstPerson(
-// 				transform,
-// 				render,
-// 				lightTexture,
-// 				hand,
-// 				pitch,
-// 				equipProgress,
-// 				swingProgress,
-// 				stack
-// 			)
-// 			return true
-// 		}
-//
-// 		return false
-// 	}
-//
-// 	fun onRenderItemFrame(transform: PoseStack?, render: MultiBufferSource?, frame: ItemFrame?, stack: ItemStack, light: Int): Boolean {
-// 		if (stack.getItem() is PrintoutItem) {
-// 			PrintoutItemRenderer.onRenderInFrame(transform, render, frame, stack, light)
-// 			return true
-// 		}
-//
-// 		return false
-// 	}
-//
-// 	fun onPlayStreaming(engine: SoundEngine?, channel: Channel?, stream: AudioStream?) {
-// 		SpeakerManager.onPlayStreaming(engine, channel, stream)
-// 	}
-//
-// 	/**
-// 	 * Add additional information about the currently targeted block to the debug screen.
-// 	 *
-// 	 * @param addText A callback which adds a single line of text.
-// 	 */
-// 	fun addBlockDebugInfo(addText: Consumer<String?>) {
-// 		val minecraft: Minecraft = Minecraft.getInstance()
-// 		if (!minecraft.options.renderDebug || minecraft.level == null) return
-// 		if (minecraft.hitResult == null || minecraft.hitResult.getType() != HitResult.Type.BLOCK) return
-//
-// 		val tile: BlockEntity? = minecraft.level.getBlockEntity((minecraft.hitResult as BlockHitResult).getBlockPos())
-//
-// 		if (tile is MonitorBlockEntity) {
-// 			addText.accept("")
-// 			addText.accept(
-// 				String.format("Targeted monitor: (%d, %d), %d x %d", tile.getXIndex(), tile.getYIndex(), tile.getWidth(), tile.getHeight())
-// 			)
-// 		} else if (tile is TurtleBlockEntity) {
-// 			addText.accept("")
-// 			addText.accept("Targeted turtle:")
-// 			addText.accept(String.format("Id: %d", tile.getComputerID()))
-// 			addTurtleUpgrade(addText, tile, TurtleSide.LEFT)
-// 			addTurtleUpgrade(addText, tile, TurtleSide.RIGHT)
-// 		}
-// 	}
-//
-// 	private fun addTurtleUpgrade(out: Consumer<kotlin.String?>, turtle: TurtleBlockEntity, side: TurtleSide?) {
-// 		val upgrade: Unit /* TODO: class org.jetbrains.kotlin.nj2k.types.JKJavaNullPrimitiveType */? = turtle.getUpgrade(side)
-// 		if (upgrade != null) out.accept(String.format("Upgrade[%s]: %s", side, upgrade.getUpgradeID()))
-// 	}
-//
-// 	/**
-// 	 * Add additional information about the game to the debug screen.
-// 	 *
-// 	 * @param addText A callback which adds a single line of text.
-// 	 */
-// 	fun addGameDebugInfo(addText: Consumer<kotlin.String?>) {
-// 		if (MonitorBlockEntityRenderer.hasRenderedThisFrame() && Minecraft.getInstance().options.renderDebug) {
-// 			addText.accept("[CC:T] Monitor renderer: " + MonitorBlockEntityRenderer.currentRenderer())
-// 		}
-// 	}
-//
-// 	@Nullable
-// 	fun getBlockBreakingState(state: BlockState, pos: BlockPos?): BlockState? {
-// 		// Only apply to cables which have both a cable and modem
-// 		if (state.getBlock() !== ModRegistry.Blocks.CABLE.get() || !state.getValue<T?>(CableBlock.CABLE) || state.getValue<T?>(CableBlock.MODEM) === CableModemVariant.None
-// 		) {
-// 			return null
-// 		}
-//
-// 		val hit: HitResult? = Minecraft.getInstance().hitResult
-// 		if (hit == null || hit.getType() != HitResult.Type.BLOCK) return null
-// 		val hitPos: BlockPos = (hit as BlockHitResult).getBlockPos()
-//
-// 		if (hitPos != pos) return null
-//
-// 		return if (WorldUtil.isVecInside(
-// 				CableShapes.getModemShape(state),
-// 				hit.getLocation().subtract(pos.getX().toDouble(), pos.getY().toDouble(), pos.getZ().toDouble())
-// 			)
-// 		)
-// 			state.getBlock().defaultBlockState().setValue<T?, V?>(CableBlock.MODEM, state.getValue<T?>(CableBlock.MODEM))
-// 		else
-// 			state.setValue<T?, T?>(CableBlock.MODEM, CableModemVariant.None)
-// 	}
 }
